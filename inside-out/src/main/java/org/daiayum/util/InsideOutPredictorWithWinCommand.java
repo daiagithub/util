6f7a4a3b9543fd2035c2cd751726e35baad1bfc9
Daia <daia.yum@gmail.com>
2015-12-08 18:22:37 +0530

    Remove unused imports

--- inside-out/src/main/java/org/daiayum/util/InsideOutPredictorWithWinCommand.java
+++ inside-out/src/main/java/org/daiayum/util/InsideOutPredictorWithWinCommand.java
@@ -1,13 +1,5 @@
 package org.daiayum.util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class InsideOutPredictorWithWinCommand extends InsideOutPredictor {
