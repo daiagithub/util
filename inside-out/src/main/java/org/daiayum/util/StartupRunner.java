7ee5a670e85682bf950333456c62ff183e67cbcf
Daia <daia.yum@gmail.com>
2015-12-09 16:55:43 +0530

    Change logging pattern

--- inside-out/src/main/java/org/daiayum/util/StartupRunner.java
+++ inside-out/src/main/java/org/daiayum/util/StartupRunner.java
@@ -1,5 +1,8 @@
 package org.daiayum.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StartupRunner {
 
 	private static final String NEW_LINE = System.getProperty("line.separator");
@@ -7,11 +10,13 @@ public class StartupRunner {
 	private static final String FILE_PATH = "F:\\tools\\recorder\\logs.log";
 	
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(StartupRunner.class);
 		AttendanceService attendanceService = new GenericAttendanceService();
     	Attendance attendance = attendanceService.registerNewAttendance(WindowsUtil.getLastBootUpTime());
     	
 		String in = AttendanceService.format(attendance.getTimeIn(), DATETIME_FORMAT);		
 		WindowsUtil.writeToFile(NEW_LINE + in, FILE_PATH);		
+		logger.info("Done.");
 	}
 
 }
