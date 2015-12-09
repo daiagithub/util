873093bcd75a24aaa4823dd2441b6e6047e8e64e
Daia <daia.yum@gmail.com>
2015-12-09 15:45:20 +0530

    Remove unused codes

--- inside-out/src/main/java/org/daiayum/util/App.java
+++ inside-out/src/main/java/org/daiayum/util/App.java
@@ -1,18 +1,7 @@
 package org.daiayum.util;
 
-import javax.swing.JOptionPane;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class App {
     public static void main( String[] args ){
-    	InsideOutPredictor insideOutPredictor = new InsideOutPredictorWithWinCommand();
-    	insideOutPredictor.setElapsedSeconds();
-    	insideOutPredictor.setAllTimeComponentsValues();
-    	String message = insideOutPredictor.generateMessage();
-    	JOptionPane.showMessageDialog(null, message);
-    	Logger logger = LoggerFactory.getLogger(App.class);
-    	logger.info("aaaa");
+    	
     }
 }
