b78256c540411d712e02b4f3c3d57a0f807de72a
Daia <daia.yum@gmail.com>
2015-12-09 15:49:52 +0530

    Update application code based on new classes addition and depricate old class.

--- inside-out/src/main/java/org/daiayum/util/ShutdownRunner.java
+++ inside-out/src/main/java/org/daiayum/util/ShutdownRunner.java
@@ -9,8 +9,14 @@ public class ShutdownRunner {
 	private static final String FILE_PATH = "F:\\tools\\recorder\\logs.log";
 	
 	public static void main(String[] args) {
-		String data = InsideOutPredictor.format(new Date(), DATETIME_FORMAT);		
-		WindowsUtil.writeToFile(TAB_CHARACTER + data, FILE_PATH);		
+		Attendance dummy = new Attendance();
+		Date timeIn = WindowsUtil.getLastBootUpTime();
+		dummy.setTimeIn(timeIn);		
+		AttendanceService attendanceService = new GenericAttendanceService();
+		attendanceService.updateAttendance(dummy);
+		
+		String out = AttendanceService.format(dummy.getTimeOut(), DATETIME_FORMAT);
+		WindowsUtil.writeToFile(TAB_CHARACTER + out + TAB_CHARACTER + dummy.getActualTimeSpent(), FILE_PATH);		
 	}
 
 }
--- inside-out/src/main/java/org/daiayum/util/StartupRunner.java
+++ inside-out/src/main/java/org/daiayum/util/StartupRunner.java
@@ -6,9 +6,12 @@ public class StartupRunner {
 	private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 	private static final String FILE_PATH = "F:\\tools\\recorder\\logs.log";
 	
-	public static void main(String[] args) {		
-		String data = InsideOutPredictor.format(WindowsUtil.getLastBootUpTime(), DATETIME_FORMAT);		
-		WindowsUtil.writeToFile(NEW_LINE + data, FILE_PATH);		
+	public static void main(String[] args) {
+		AttendanceService attendanceService = new GenericAttendanceService();
+    	Attendance attendance = attendanceService.registerNewAttendance(WindowsUtil.getLastBootUpTime());
+    	
+		String in = AttendanceService.format(attendance.getTimeIn(), DATETIME_FORMAT);		
+		WindowsUtil.writeToFile(NEW_LINE + in, FILE_PATH);		
 	}
 
 }
