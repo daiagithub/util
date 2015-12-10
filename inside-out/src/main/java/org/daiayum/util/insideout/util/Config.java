3eb8c074c50963124d177cb08990be8ab5003158
Daia <daia.yum@gmail.com>
2015-12-10 20:34:58 +0530

    Change method call

--- inside-out/src/main/java/org/daiayum/util/insideout/util/Config.java
+++ inside-out/src/main/java/org/daiayum/util/insideout/util/Config.java
@@ -2,10 +2,10 @@ package org.daiayum.util.insideout.util;
 
 public class Config {
 	
-	public static final String TAB_CHARACTER = (String) PropertiesLoader.PROPERTIES.get("tab.character");
-	public static final String ATTENDANCE_LOG_FILEPATH = (String) PropertiesLoader.PROPERTIES.get("attendance.log.filepath");
-	public static final String ATTENDANCE_DATETIME_FORMAT = (String) PropertiesLoader.PROPERTIES.get("attendance.datetime.format");
-	public static final String ATTENDANCE_TIME_FORMAT = (String) PropertiesLoader.PROPERTIES.get("attendance.time.format");
-	public static final String WINDOW_COMMAND_LASTBOOTUPTIME = (String) PropertiesLoader.PROPERTIES.get("windows.command.lastbootuptime");
+	public static final String TAB_CHARACTER = PropertiesLoader.PROPERTIES.getProperty("tab.character");
+	public static final String ATTENDANCE_LOG_FILEPATH = PropertiesLoader.PROPERTIES.getProperty("attendance.log.filepath");
+	public static final String ATTENDANCE_DATETIME_FORMAT = PropertiesLoader.PROPERTIES.getProperty("attendance.datetime.format");
+	public static final String ATTENDANCE_TIME_FORMAT = PropertiesLoader.PROPERTIES.getProperty("attendance.time.format");
+	public static final String WINDOW_COMMAND_LASTBOOTUPTIME = PropertiesLoader.PROPERTIES.getProperty("windows.command.lastbootuptime");
 	
 }
