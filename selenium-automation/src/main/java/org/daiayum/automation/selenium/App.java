ec45d187cfddbccdb2995eedc7637f33b7e671df
Daia Yum <daia.yum@gmail.com>
2016-02-01 19:03:53 +0530

    Initial commit

---
+++ selenium-automation/src/main/java/org/daiayum/automation/selenium/App.java
@@ -0,0 +1,12 @@
+package org.daiayum.automation.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
